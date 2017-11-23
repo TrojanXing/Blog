e98822d663557b3cfd8a1151917aa3303e49b804
xing <xingli@usc.edu>
2017-11-22 21:43:03 -0800

    Change route hide password for get all users

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,13 +1,47 @@
 const User = require('../../models/user');
-const mongoose = require('mongoose');
 const router = require('express').Router();
 const verifyToken = require('../routesMiddleware').verifyToken;
 
+router.get('/publicProfile/:username', (req, res) => {
+  if(!req.params.username) {
+    res.json({ success: false, message: 'username is not provided'});
+  } else {
+    User.find({ username: req.params.username })
+      .select('username email')
+      .exec((err, user) => {
+        if (err) {
+          res.json({ success: false, message: 'Something wrong with the database'});
+        } else {
+          if (!user) {
+            res.json({ success: false, message: 'Cannot get user with given name'});
+          } else {
+            res.json({ success: true, user: user});
+          }
+        }
+      });
+  }
+});
+
+router.get('/allUsers', (req, res) => {
+  User.find({})
+    .select('username email')
+    .exec((err, users) => {
+    if (err) {
+      res.json({ success: false, message: err })
+    } else {
+      if (!users) {
+        res.json({ success: false, message: 'Users Not Found'});
+      } else {
+        res.json({  success: true, users: users });
+      }
+    }
+  });
+});
 /**
  * Middleware
  * Grab token, all operation need auth blow this
  */
-router.use((req, res, next) => verifyToken(req, res, next));
+// router.use((req, res, next) => verifyToken(req, res, next));
 
 /**
  * Get user profile
@@ -26,21 +60,5 @@ router.get('/profile', (req, res) => {
     }
   })
 });
-
-router.get('/allUsers', (req, res) => {
-  User.find({}, (err, users) => {
-    if (err) {
-      res.json({ success: false, message: err })
-    } else {
-      if (!users) {
-        res.json({ success: false, message: 'Users Not Found'});
-      } else {
-        res.json({  success: true, users: users });
-      }
-    }
-  });
-});
-
-
 
 module.exports = router;
